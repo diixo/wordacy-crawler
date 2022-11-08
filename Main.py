421285418b81e24968f1bd3b05669b147ff95ecd
diixo <anatolii.podoba@gmail.com>
2022-11-08 11:39:51 +0200

    fixed compile errors

--- Main.py
+++ Main.py
@@ -8,28 +8,28 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from urllib.request import Request
 
-url = "https://www.datasciencecentral.com/top-10-projects-for-data-science-and-machine-learning/"
+#url = "https://www.datasciencecentral.com/top-10-projects-for-data-science-and-machine-learning/"
 #url = "https://www.techopedia.com/definition/26184/c-plus-plus-programming-language"
 ####################################################################################################
 # https://understandingdata.com/python-for-seo/how-to-extract-text-from-multiple-webpages-in-python/
 # https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
 
-url = "https://www.labri.fr/perso/nrougier/from-python-to-numpy/"
-
+#url = "https://www.labri.fr/perso/nrougier/from-python-to-numpy/"
+url = "https://www.ibm.com/cloud/blog/supervised-vs-unsupervised-learning/"
 
 train_db = open("train-db.txt", 'w', encoding='utf-8')
-
-# kill all root-nodes in DOM-model: script and style elements
-for node in raw(["script", "style", "header", "footer", "noscript", "iframe", "svg", "button", "img", "span"]):
-    node.extract()   # cut it out
-
 
 def parseURL(url, train_db):
     req = Request(url, headers={'User-Agent': 'XYZ/3.0'})
     html = urllib.request.urlopen(req).read()
     raw = BeautifulSoup(html, features="html.parser")
+
+    # kill all root-nodes in DOM-model: script and style elements
+    for node in raw(["script", "style", "header", "footer", "noscript", "iframe", "svg", "button", "img", "span"]):
+        node.extract()  # cut it out
     
     parse(raw, train_db)
+    return
 
     # get text
     text = raw.get_text()
@@ -50,15 +50,18 @@ def parse(soup, train_db):
     for item in soup(["h1", "h2", "h3", "h4", "p", "a", "li"]):
         if item.name == 'h1':
             h1 = True
+        if item.name == 'li':
+            txt = item.get_text()
+            print(txt)
         if h1 == True:
             txt = item.get_text().replace('\n', ' ')
             print(txt)
             
- def parseFile(filename, train_db):
-    soup = BeautifulSoup(open(filename, encoding='utf-8'), "html.parser")
-    parse(soup, train_db)
+def parseFile(filename, train_db):
+    raw = BeautifulSoup(open(filename, encoding='utf-8'), "html.parser")
+    parse(raw, train_db)
             
- def list_dir(dir_path):
+def list_dir(dir_path, train_db):
     i = 0
     for root, dirs, files in os.walk(dir_path):
         for file in files:
@@ -69,4 +72,6 @@ def parse(soup, train_db):
                 print(i)
 
 # clean raw text
-parse(url, "")
+parseURL(url, train_db)
+
+train_db.close()

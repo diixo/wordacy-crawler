dfa413337892462d9cd66d147fe4a20348df822b
diixo <anatolii.podoba@gmail.com>
2022-11-11 18:14:10 +0200

    Full implementation the content writing to train_db.

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ from urllib.request import Request
 
 #url = "https://www.datasciencecentral.com/top-10-projects-for-data-science-and-machine-learning/"
 #url = "https://www.techopedia.com/definition/26184/c-plus-plus-programming-language"
-####################################################################################################
+########################################################################
 # https://understandingdata.com/python-for-seo/how-to-extract-text-from-multiple-webpages-in-python/
 # https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
 
@@ -20,13 +20,36 @@ url = "https://www.datasciencecentral.com/category/technical-topics/data-science
 
 train_db = open("train-db.txt", 'w', encoding='utf-8')
 
+########################################################################
 def processString(str):
     txt = str.replace('\n', ' ')
     txt = txt.replace('\t', ' ')
     txt = txt.replace('\u00a0', ' ')
     txt = txt.strip()  # trim spaces
     return txt
-
+########################################################################
+def writeElement(item, train_db):
+    txt = processString(item.get_text())
+    if txt:
+        print("<" + item.name + "> " + txt)
+        train_db.write("<" + item.name + ">\n")
+        train_db.write(txt)
+        train_db.write("\n</" + item.name + ">\n")
+########################################################################
+def writeTitleElement(item, text, train_db):
+    txt = processString(item.get_text())
+    title = processString(text)
+    if txt:
+        print("<" + item.name + "> " + txt)
+        train_db.write("<" + item.name + ">\n")
+        train_db.write(txt + " : " + title)
+        train_db.write("\n</" + item.name + ">\n")
+    else:
+        print("<" + item.name + "> :")
+        train_db.write("<" + item.name + ">\n")
+        train_db.write(": " + title)
+        train_db.write("\n</" + item.name + ">\n")
+########################################################################
 def parseURL(url, train_db):
     req = Request(url, headers={'User-Agent': 'XYZ/3.0'})
     html = urllib.request.urlopen(req).read()
@@ -38,17 +61,18 @@ def parseURL(url, train_db):
 
     header = raw.find_all("header")
     #proceed header-section separetely
+    train_db.write("<header>\n")
     for h in header:
         ha = h.find_all("a")
         for a in ha:
-            a_txt = processString(a.get_text())
-            if a_txt: print("<" + a.name + "> " + a_txt)
+            #a_txt = processString(a.get_text())
+            writeElement(a, train_db)
             a.extract()
 
         hli = h.find_all("li")
         for li in hli:
-            li_txt = processString(li.get_text())
-            if li_txt: print("<" + li.name + "> " + li_txt)
+            writeElement(li, train_db)
+    train_db.write("</header>\n")
 
     # kill all root-nodes in DOM-model: script and style elements
     for node in raw(blacklist):
@@ -70,10 +94,12 @@ def parseURL(url, train_db):
     text = '\n'.join(chunk for chunk in chunks if chunk)
 
     print(text)
-
+########################################################################
 def parse(soup, train_db):
     h1 = False
     txt = ""
+
+    train_db.write("<body>\n")
 
     for item in soup(["h1", "h2", "h3", "h4", "p", "a", "li"]):
         if item.name == 'h1':
@@ -89,16 +115,16 @@ def parse(soup, train_db):
 
         if hasattr(item, 'attrs'):
             if 'title' in item.attrs:
-                title = item.attrs['title']
-                title = processString(title)
-                print(txt + " : " + title)
-            else: print(txt)
-        else: print(txt)
+                writeTitleElement(item, item.attrs['title'], train_db)
+            else: writeElement(item, train_db)
+        else: writeElement(item, train_db)
 
+    train_db.write("</body>\n")
+########################################################################
 def parseFile(filename, train_db):
     raw = BeautifulSoup(open(filename, encoding='utf-8'), "html.parser")
     parse(raw, train_db)
-            
+########################################################################
 def list_dir(dir_path, train_db):
     i = 0
     for root, dirs, files in os.walk(dir_path):
@@ -108,6 +134,7 @@ def list_dir(dir_path, train_db):
                 parseFile(os.path.join(root, file), train_db)
                 i += 1
                 print(i)
+########################################################################
 
 # clean raw text
 parseURL(url, train_db)

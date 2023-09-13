b22f9e37b81232b7adebc202973291f14a774d03
diixo <anatolii.podoba@gmail.com>
2023-09-14 00:38:06 +0300

    updated manager

--- manager_qq.py
+++ manager_qq.py
@@ -1,3 +1,17 @@
 
-class manager_qq:
-   pass
+from pathlib import Path
+
+class Manager:
+
+   def __init__(self):
+
+      self.urls = set()
+
+   def load_data(self):
+      rel = "./data/"
+
+      path = Path(rel + "urls.txt")
+      if path.exists():
+         self.urls.update([line.replace('\n', '')
+            for line in open(rel + path.name, 'r', encoding='utf-8').readlines()])
+

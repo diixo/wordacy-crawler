ffdf1d55260683bc55f14a9e13651042f5bf544c
diixo <anatolii.podoba@gmail.com>
2023-09-14 11:39:31 +0300

    manager.load(json)

--- manager_qq.py
+++ manager_qq.py
@@ -1,4 +1,5 @@
 
+import json
 from pathlib import Path
 
 class Manager:
@@ -6,12 +7,22 @@ class Manager:
    def __init__(self):
 
       self.urls = set()
+      self.content = dict()
 
-   def load_data(self):
+   def load(self):
       rel = "./data/"
 
       path = Path(rel + "urls.txt")
       if path.exists():
          self.urls.update([line.replace('\n', '')
             for line in open(rel + path.name, 'r', encoding='utf-8').readlines()])
+
+      path = Path("./storage/data.json")
+      if path.exists():
+         fd = open("./storage/data.json", 'r', encoding='utf-8')
+         self.content = json.load(fd)
+
+if __name__ == "__main__":
+   manager = Manager()
+   manager.load()
 

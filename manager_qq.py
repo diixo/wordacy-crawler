6d3c5f44331bef7e5605b1c36ab75e02cd39ee3d
diixo <anatolii.podoba@gmail.com>
2023-09-14 16:45:23 +0300

    Manager refactoring: load/save from json. Start stage.

--- manager_qq.py
+++ manager_qq.py
@@ -1,6 +1,7 @@
 
 import json
 from pathlib import Path
+import parser_qq as qq
 
 class Manager:
 
@@ -9,20 +10,48 @@ class Manager:
       self.urls = set()
       self.content = dict()
 
-   def load(self):
-      rel = "./data/"
+   def load_storage(self):
+      rel = "./storage/"
 
-      path = Path(rel + "urls.txt")
+      path = Path(rel + "data.json")
       if path.exists():
-         self.urls.update([line.replace('\n', '')
-            for line in open(rel + path.name, 'r', encoding='utf-8').readlines()])
-
-      path = Path("./storage/data.json")
-      if path.exists():
-         fd = open("./storage/data.json", 'r', encoding='utf-8')
+         fd = open(rel + path.name, 'r', encoding='utf-8')
          self.content = json.load(fd)
 
+      path = Path(rel + "urls.json")
+      if path.exists():
+         fd = open(rel + path.name, 'r', encoding='utf-8')
+         self.urls = set(json.load(fd))
+
+   def learn(self, url: str):
+      url = url.lower().strip('/')
+      if url in self.urls:
+         print(f"url={url} already")
+      else:
+         qq.parse_url(url, self.content)
+         self.urls.add(url)
+
+   def save_storage(self):
+      rel = "./storage/"
+
+      filename="data.json"
+      qq.save_json(self.content, rel + filename)
+
+      filename="urls.json"
+      with open(rel + filename, 'w', encoding='utf-8') as fd:
+         json.dump(list(self.urls), fd, ensure_ascii=False, indent=3)
+
+
+   def analyse(self, url: str):
+      pass
+
 if __name__ == "__main__":
+   u1 = "https://pythonexamples.org/"
+   u2 = "https://kotlinandroid.org/"
+
    manager = Manager()
-   manager.load()
+   manager.load_storage()
+   manager.learn(u1)
+   manager.learn(u2)
+   manager.save_storage()
 

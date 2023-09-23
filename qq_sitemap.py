a86deb5180cef6f82345f4eef37c54f4e34b753a
diixo <anatolii.podoba@gmail.com>
2023-09-23 19:54:38 +0300

    initial commit techopedia by sitemap

--- qq_sitemap.py
+++ qq_sitemap.py
@@ -1,16 +1,31 @@
 
+import json
+from urllib.parse import urlparse
 from usp.tree import sitemap_tree_for_homepage
 
+
+def save_json(hostname:str, result:dict):
+   filepath = "./storage/" + hostname + ".json"
+   with open(filepath, 'w', encoding='utf-8') as fd:
+      json.dump(result, fd, ensure_ascii=False, indent=3)
+
+
 def main():
-   tree = sitemap_tree_for_homepage('https://www.programiz.com')
-   #tree = sitemap_tree_for_homepage('https://www.techopedia.com')
 
-   counter = 0;
-   for page in tree.all_pages():
-      counter += 1
-      #print(page)
+   url = str.strip("https://www.techopedia.com", '/')
+   filter = ["/contributors/", "/companies/", "/contact", "/advertise", "/about/", "/subscribe"]
 
-   print(f"sz={counter}")
+   tree = sitemap_tree_for_homepage(url)
+   filter = set([url + "/" + f for f in filter])
+
+   urls = [str(page.url).strip() for page in tree.all_pages()]
+
+   print(f"sz={len(urls)}")
+
+   result = dict()
+   result[url] = urls
+
+   save_json(urlparse(url).hostname, result=result)
 
 ##########################
 if __name__ == "__main__":

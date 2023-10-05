3a9ff99c9cb3dcf84a09dd8dc87af1183139fae8
diixo <anatolii.podoba@gmail.com>
2023-10-05 11:55:22 +0300

    small...

--- qq_sitemap.py
+++ qq_sitemap.py
@@ -3,20 +3,17 @@ import json
 from urllib.parse import urlparse
 from usp.tree import sitemap_tree_for_homepage
 
-
 def save_json(hostname:str, result:dict):
    filepath = "./storage/" + hostname + ".json"
    with open(filepath, 'w', encoding='utf-8') as fd:
       json.dump(result, fd, ensure_ascii=False, indent=3)
 
 def make_sitemap(url, filter=[]):
-
    url = str.strip(url, '/')
+   urls = set()
 
    filter = set([(url + f) for f in filter])
    tree = sitemap_tree_for_homepage(url)
-
-   urls = set()
 
    for page in tree.all_pages():
       for f in filter:

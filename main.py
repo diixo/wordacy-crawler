f258058ada19d9f549e48bede91f2c0338effa52
diixo <anatolii.podoba@gmail.com>
2023-10-20 07:58:30 +0300

    update main.py

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import os
 import io
 import re
 
+from qq_analyzer import Analyzer
+from qq_prediction import Prediction
+
 
 def main():
-    print("<<")
+    analyzer = Analyzer()
+    analyzer.open_json("storage/allainews-news.json")
+    result = analyzer.content.get("data", dict())
+    print(len(result.keys()))
 
 
 if __name__ == "__main__":

caed1bd24f64595bdd08e91509ddf6ae561c03e0
diixo <anatolii.podoba@gmail.com>
2023-10-22 21:21:13 +0300

    moved str_to_ngrams to qq_grammar.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from qq_analyzer import Analyzer
 from qq_prediction import Prediction
 import qq_parser as qq
+import qq_grammar as qq_grammar
 
 
 def load_stopwords():
@@ -15,36 +16,6 @@ def load_stopwords():
     if diix.exists():
         return set([line.replace('\n', '') for line in open(str(diix), 'r', encoding='utf-8').readlines()])
     return set()
-
-
-def str_to_ngrams(str_line: str, stopwords: set()):
-
-    line1 = str_line.replace(". ", "! ")
-    line1 = re.sub('[!?;,:\[\]\(\)]', "!", line1)
-    strips = [x.strip() for x in line1.split("!") if x !='']
-
-    punctuation = " ©®-%$!?:,;\'\" @~&()=*_<=>{|}[/]^\\"
-    result = []
-    exclude = { "2d", "3d", "3g", "4g", "5g", "6g" }
-
-    for item in strips:
-
-        #words_list = [x.strip(" ") for x in item.split(" ") if (x != '')]
-        #words_list = [x.strip(punctuation) if x not in self.dictionary else x for x in item.split(" ") if (x != '')]
-
-        word_list = [x.strip(punctuation) for x in item.split(" ") if (x.strip(punctuation) != '')]
-        tokens = []
-        for w in word_list:
-            wlow = w.lower()
-            if (w == "IT") or (wlow == "c#") or (wlow in exclude):
-                tokens.append(wlow)
-            else:
-                wlow = wlow.strip("#")
-                if qq.is_word(wlow, stopwords):
-                    tokens.append(wlow)
-
-        if tokens: result.append(tokens)
-    return result
 
 
 def test_prediction():
@@ -60,7 +31,7 @@ def test_prediction():
     prediction = Prediction()
     for string in content.keys():
         string = qq.translate(string)
-        ngrams = str_to_ngrams(string, stopwords)
+        ngrams = qq_grammar.str_to_ngrams(string, stopwords)
         for tokens in ngrams:
             prediction.add_tokens(tokens)
 
--- qq_grammar.py
+++ qq_grammar.py
@@ -1,3 +1,33 @@
+import re
+import qq_parser as qq
 
 s = "John's mom went there, but he wasn't c++, c#, .net, Q&A/Q-A, #nope i_t IT at-all'. So' she said: 'Where are& viix.co. !!' 'A a'"
 
+def str_to_ngrams(str_line: str, stopwords: set()):
+   
+    line1 = str_line.replace(". ", "! ")
+    line1 = re.sub('[!?;,:\[\]\(\)]', "!", line1)
+    strips = [x.strip() for x in line1.split("!") if x !='']
+
+    punctuation = " ©®-%$!?:,;\'\" @~&()=*_<=>{|}[/]^\\"
+    result = []
+    exclude = { "2d", "3d", "3g", "4g", "5g", "6g" }
+
+    for item in strips:
+
+        #words_list = [x.strip(" ") for x in item.split(" ") if (x != '')]
+        #words_list = [x.strip(punctuation) if x not in self.dictionary else x for x in item.split(" ") if (x != '')]
+
+        word_list = [x.strip(punctuation) for x in item.split(" ") if (x.strip(punctuation) != '')]
+        tokens = []
+        for w in word_list:
+            wlow = w.lower()
+            if (w == "IT") or (wlow == "c#") or (wlow in exclude):
+                tokens.append(wlow)
+            else:
+                wlow = wlow.strip("#")
+                if qq.is_word(wlow, stopwords):
+                    tokens.append(wlow)
+
+        if tokens: result.append(tokens)
+    return result

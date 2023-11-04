8cf97e0d28e89145dea1a792c5ba081c828b8724
diixo <anatolii.podoba@gmail.com>
2023-11-04 08:58:01 +0200

    impl is_date(): extended filter, removed dates from prediction-dataset.

--- qq_grammar.py
+++ qq_grammar.py
@@ -9,6 +9,11 @@ def is_complex_digit(word: str):
     w = re.sub(r'[$]?[-+]?[\d]*[.,\:]?[\d]+[ %\"\'\)\+]*[A-Za-z0-9]?', "", word)
     return not w
 
+def is_date(value: str):
+    # check formats: DD:MM:YYYY, DD.MM.YYYY, DD-MM-YYYY, DD/MM/YYYY
+    pattern = r"^\d{2}(:|\.|\-|\/)\d{2}\1\d{4}$"
+    return re.match(pattern, value) != None
+
 def str_tokenize_words(s: str):
     s = re.findall("(\.?\w[\w'\.&-]*\w|\w\+*#?)", s)
     if s: return s
@@ -17,7 +22,7 @@ def str_tokenize_words(s: str):
 def is_word(word: str, stopwords=set()):
     #word = re.search("[\[\]\}\{=@\*]")
     if (re.sub("[A-Za-z0-9#\'\./_&+-]", "", word) == "") and len(word) > 1:
-        if ((word not in stopwords) and not word.isdigit() and not is_complex_digit(word)):
+        if ((word not in stopwords) and not word.isdigit() and not is_complex_digit(word)) and not is_date(word):
             return True
     return False
 
@@ -71,6 +76,8 @@ def str_to_ngrams(str_line: str, stopwords: set()):
 ##########################################
 
 if __name__ == "__main__":
+
+    print(is_date("00.01.2000"), is_date("00-01-2000"), is_date("00/01/2000"), is_date("00:01:2000"))
 
     d_test = [ "160", "160)", "160.0", "+160", "+160.0", "$0.2%", "$.225%", "$.225%", 
                 "$.225%", "$.225%%", "$+.225%", "$,225%", "$:225%", "$+55%%%" ]

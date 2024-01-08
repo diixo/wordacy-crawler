9bfbadefb6519f3c0649a5ad032d63df21805d26
diixo <anatolii.podoba@gmail.com>
2024-01-09 01:11:00 +0200

    SearchTest: qq_test_search.py small refactoring

--- qq_test_search.py
+++ qq_test_search.py
@@ -10,6 +10,18 @@ class SearchTest:
         self.bigrams = set()
         self.trigrams = set()
 
+        with open('./storage/allainews-news.json', 'r', encoding='utf-8') as fd:
+            content = json.load(fd)
+            keywords = content.get("keywords", list())
+            ##########################################################################
+
+            for string in keywords:
+                grams = qq.str_tokenize_words(string)
+                self.add_tokens(grams)
+        
+        ##########################################################################
+        print(f"uni={len(self.unigrams)}, bi={len(self.bigrams)}, tri={len(self.trigrams)}")
+
     def add_tokens(self, tokens: list):
         sz = len(tokens)
         if sz == 1:
@@ -24,25 +36,24 @@ class SearchTest:
             ngrams_3 = qq.ngrams(tokens, 3)
             self.trigrams.update(ngrams_3)
 
-    def set_keywords(self):
-        stopwords = set()
 
-        with open('./storage/allainews-news.json', 'r', encoding='utf-8') as fd:
-            content = json.load(fd)
-            keywords = content.get("keywords", list())
-            ##########################################################################
-
-            for string in keywords:
-                grams = qq.str_tokenize_words(string)
-                self.add_tokens(grams)
+    def calculate_tags(self, heading, stopwords=set()):
+        grams = qq.str_tokenize_words(heading.lower())
+        result = []
+        #for tokens in grams:
+        ngrams_1 = qq.ngrams(grams, 1)
+        result.extend([" ".join(gram) for gram in ngrams_1 if gram in self.unigrams])
         
-        ##########################################################################
-        print(f"uni={len(self.unigrams)}, bi={len(self.bigrams)}, tri={len(self.trigrams)}")
+        ngrams_2 = qq.ngrams(grams, 2)
+        result.extend([" ".join(gram) for gram in ngrams_2 if gram in self.bigrams])
+        
+        ngrams_3 = qq.ngrams(grams, 3)
+        result.extend([" ".join(gram) for gram in ngrams_3 if gram is self.trigrams])
+        # TODO: unique results
+        return result
 
 def main():
-    keywords = ["data science", "neural networks", "ai news"]
     test = SearchTest()
-    test.set_keywords()
 
 if __name__ == '__main__':
     main()

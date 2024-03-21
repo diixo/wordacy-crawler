1dc773d3b290c016f00f94ccc8a68369653baf29
diixo <anatolii.podoba@gmail.com>
2024-03-21 15:30:47 +0200

    app-make-dataset.py updated.

--- app-make-dataset.py
+++ app-make-dataset.py
@@ -4,6 +4,24 @@ import time
 from pathlib import Path
 
 from qq_analyzer import Analyzer
+import qq_grammar as qq
+
+
+f = Path("data/db-full.txt")
+diixonary = set()
+if f.exists():
+    diixonary = set([line.replace('\n', '') for line in open(str(f), 'r', encoding='utf-8').readlines()])
+print(len(diixonary))
+
+f = Path("make-dataset/train.devices.txt")
+if f.exists():
+    for line in open(str(f), 'r', encoding='utf-8').readlines():
+        words = qq.str_tokenize_words(line.lower())
+        result = [w for w in words if (w not in diixonary and not qq.is_digit(w))]
+        print(result)
+
+print(result)
+exit(0)
 
 analyzer = Analyzer()
 analyzer.open_json("make-dataset/dataset.json")

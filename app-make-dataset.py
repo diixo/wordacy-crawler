499f7a71a68ab4cf208c63a44eb684bd160347af
diixo <anatolii.podoba@gmail.com>
2024-03-23 02:44:19 +0200

    app-make-dataset updated

--- app-make-dataset.py
+++ app-make-dataset.py
@@ -3,43 +3,60 @@ import json
 import time
 from pathlib import Path
 
-from qq_analyzer import Analyzer
+
 import qq_grammar as qq
+from qq_analyzer import Analyzer
+from qq_crawler2 import Crawler2
 
 
-f = Path("data/db-full.txt")
-diixonary = set()
-if f.exists():
-    diixonary = set([line.replace('\n', '') for line in open(str(f), 'r', encoding='utf-8').readlines()])
-print(len(diixonary))
-
-f = Path("make-dataset/train.devices.txt")
-if f.exists():
-    for line in open(str(f), 'r', encoding='utf-8').readlines():
-        line = qq.translate(line.lower())
-        words = qq.str_tokenize_words(line)
-        result = [w for w in words if (w not in diixonary and not qq.is_digit(w))]
-        print(result)
-
-print(result)
-exit(0)
-
-analyzer = Analyzer()
-analyzer.open_json("make-dataset/dataset.json")
-
-def load_urls():
-    f = Path("make-dataset/urls.txt")
+def process_file():
+    f = Path("data/db-full.txt")
+    diixonary = set()
     if f.exists():
-        return [line.replace('\n', '') for line in open(str(f), 'r', encoding='utf-8').readlines()]
-    return []
+        diixonary = set([line.replace('\n', '') for line in open(str(f), 'r', encoding='utf-8').readlines()])
+    print(len(diixonary))
 
-urls = load_urls()
+    f = Path("make-dataset/dataset.txt")
+    if f.exists():
+        for line in open(str(f), 'r', encoding='utf-8').readlines():
+            line = qq.translate(line.lower())
+            words = qq.str_tokenize_words(line)
+            result = [w for w in words if (w not in diixonary and not qq.is_digit(w))]
+            print(result)
 
-print(f">> [Analyzer] :{len(analyzer.content.get('headings', dict()))}")
-for u in urls:
-    if analyzer.learn_url(u, hhh_mask=["h1", "H1"]):
-        print(f"[Analyzer] ...on [{len(urls)}]: {u}")
-        time.sleep(2.0)
-analyzer.save_json()
-print(f"<< [Analyzer] :{len(analyzer.content.get('headings', dict()))}")
+    print(result)
 
+
+def test_url_to_dataset():
+    url = "https://www.simplilearn.com"
+
+    crawler = Crawler2(recursive=True)
+    crawler.enqueue_url(url)
+    crawler.set_filter(url, [
+      "/about-us",
+      "/careers",
+      "/contact-us",
+      "/feed",
+      "/guest-blogging",
+      "/terms-and-conditions",
+
+      "/terms/", 
+      "/privacy/", 
+      "/accounts/", 
+      "/filtered/", 
+      "/videos/", 
+      "/feed/", 
+      "/topic/",
+      "/source/",
+      "/news/feed",
+      "/term",
+      "/about",
+      "/podcasts",
+      "/sources.md"
+      ])
+    crawler.run()
+    crawler.save_json("test/simplilearn.com.json")
+
+if __name__ == "__main__":
+    #exit(0)
+    test_url_to_dataset()
